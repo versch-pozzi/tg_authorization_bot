bc3779a08127b4b5caff7083e87580c6a3e91e52
Versch <snozthk@gmail.com>
2025-03-23 23:14:42 +0300

    Update proverka.py

--- proverka.py
+++ proverka.py
@@ -25,7 +25,7 @@ def create_table_if_not_exists():
         cursor.close()
         conn.close()
     except Exception as e:
-        print(f"Ошибка при создании таблицы: {e}")
+        print(f"Error creating table: {e}")
 
 
 create_table_if_not_exists()
@@ -34,39 +34,39 @@ create_table_if_not_exists()
 def start(message):
     user_id = message.chat.id
     if check_user_id_in_db(user_id):
-        bot.send_message(user_id, "Вы уже зарегистрированы! Можете продолжать работу с ботом.")
+        bot.send_message(user_id, "You are already registered! You can continue working with the bot.")
     else:
         keyboard = types.InlineKeyboardMarkup()
-        button1 = types.InlineKeyboardButton(text="Регистрация", callback_data="button1")
-        button2 = types.InlineKeyboardButton(text="Вход", callback_data="button2")
+        button1 = types.InlineKeyboardButton(text="Registration", callback_data="button1")
+        button2 = types.InlineKeyboardButton(text="Login", callback_data="button2")
         keyboard.add(button1)
         keyboard.add(button2)
-        bot.send_message(user_id, "Выберите способ входа", reply_markup=keyboard)
+        bot.send_message(user_id, "Select a login method", reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data == "button1":
-        msg = bot.send_message(call.from_user.id, "Хорошо, создайте себе никнейм")
+        msg = bot.send_message(call.from_user.id, "Okay, create yourself a nickname.")
         bot.register_next_step_handler(msg, process_username)
         
     elif call.data == "button2":
-        msg = bot.send_message(call.from_user.id, "Хорошо, введите ваш никнейм")
+        msg = bot.send_message(call.from_user.id, "Ok, enter your nickname")
         bot.register_next_step_handler(msg, process_login_username)
 
 def process_username(message):
     try:
         username = message.text.strip()
         if check_user_in_db(username):
-            bot.send_message(message.chat.id, 'Этот никнейм уже занят!')
+            bot.send_message(message.chat.id, 'This nickname is already taken!')
             return start(message)
             
         user_data[message.chat.id] = {'username': username}
-        msg = bot.send_message(message.chat.id, 'Теперь придумайте пароль')
+        msg = bot.send_message(message.chat.id, 'Now create a password')
         bot.register_next_step_handler(msg, process_password)
         
     except Exception as e:
         print(e)
-        bot.reply_to(message, 'Произошла ошибка!')
+        bot.reply_to(message, 'An error has occurred!')
 
 def process_password(message):
     try:
@@ -74,27 +74,27 @@ def process_password(message):
         username = user_data[message.chat.id]['username']
         user_id = message.chat.id
         reg(username, password, user_id)
-        bot.send_message(message.chat.id, 'Вы успешно зарегистрировались!')
+        bot.send_message(message.chat.id, 'You have registered successfully!')
         del user_data[message.chat.id]
         
     except Exception as e:
         print(e)
-        bot.reply_to(message, 'Произошла ошибка!')
+        bot.reply_to(message, 'An error has occurred!')
 
 def process_login_username(message):
     try:
         username = message.text.strip()
         if not check_user_in_db(username):
-            bot.send_message(message.chat.id, 'Пользователь не найден!')
+            bot.send_message(message.chat.id, 'User not found!')
             return start(message)
             
         user_data[message.chat.id] = {'username': username}
-        msg = bot.send_message(message.chat.id, 'Введите ваш пароль')
+        msg = bot.send_message(message.chat.id, 'Enter your password')
         bot.register_next_step_handler(msg, process_login_password)
         
     except Exception as e:
         print(e)
-        bot.reply_to(message, 'Произошла ошибка!')
+        bot.reply_to(message, 'An error has occurred!')
 
 def process_login_password(message):
     try:
@@ -104,21 +104,21 @@ def process_login_password(message):
         if check_password(username, password):
             user_id = message.chat.id
             update_user_id_in_db(username, user_id)
-            bot.send_message(message.chat.id, 'Успешный вход!')
+            bot.send_message(message.chat.id, 'Successful login!')
         else:
-            bot.send_message(message.chat.id, 'Неверный пароль!')
+            bot.send_message(message.chat.id, 'Incorrect password!')
             
         del user_data[message.chat.id]
         
     except Exception as e:
         print(e)
-        bot.reply_to(message, 'Произошла ошибка!')
+        bot.reply_to(message, 'An error has occurred!')
 
 
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
-                                                #Работа с бд#
+                                               #Working with DB#
 
 def reg(username, password, user_id):
     try:
